f8c9552bfbdd3e1bf6b90cafa79c37afb81ff5bc
Christopher Lloyd <cjwl@objc.net>
2010-05-21 16:44:50 -0400

    - attempt at fixing flickering NSOpenGLView on Windows 7 - added opengl32 as a direct dependency - added CGGradient skeleton - reorganized some private CA, FreeBSD CGL fixes

---
+++ AppKit/Win32.subproj/opengl_dll.m
@@ -0,0 +1,142 @@
+#import "opengl_dll.h"
+#import <Foundation/NSString.h>
+
+/* This indirection is so that applications which don't use the NSOpenGL* classes don't have to link or load opengl32.dll just because the AppKit might use it. This should probably get reworked into a CGL interface
+ */
+HGLRC opengl_wglCreateContext(HDC dc) {
+   typedef WINGDIAPI HGLRC WINAPI (*ftype)(HDC);
+   static ftype                     function=NULL;
+   
+   if(function==NULL){
+    HANDLE library=LoadLibrary("OPENGL32");
+    
+    function=(ftype)GetProcAddress(library,"wglCreateContext");
+   }
+   if(function==NULL)
+    return NULL;
+    
+   return function(dc);
+}
+
+BOOL  opengl_wglDeleteContext(HGLRC hglrc) {
+   typedef WINGDIAPI BOOL WINAPI (*ftype)(HGLRC);
+   static ftype                    function=NULL;
+   
+   if(function==NULL){
+    HANDLE library=LoadLibrary("OPENGL32");
+    
+    function=(ftype)GetProcAddress(library,"wglDeleteContext");
+   }
+   if(function==NULL)
+    return NO;
+    
+   return function(hglrc);
+}
+
+HGLRC opengl_wglGetCurrentContext(void) {
+   typedef WINGDIAPI HGLRC WINAPI (*ftype)(void);
+   static ftype                     function=NULL;
+   
+   if(function==NULL){
+    HANDLE library=LoadLibrary("OPENGL32");
+    
+    function=(ftype)GetProcAddress(library,"wglGetCurrentContext");
+   }
+   if(function==NULL)
+    return NULL;
+    
+   return function();
+}
+
+BOOL  opengl_wglMakeCurrent(HDC dc,HGLRC hglrc) {
+   typedef WINGDIAPI BOOL WINAPI (*ftype)(HDC,HGLRC);
+   static ftype                    function=NULL;
+
+   if(function==NULL){
+    HANDLE library=LoadLibrary("OPENGL32");
+    
+    function=(ftype)GetProcAddress(library,"wglMakeCurrent");
+   }
+   if(function==NULL)
+    return NO;
+
+   return function(dc,hglrc);
+}
+
+PROC  opengl_wglGetProcAddress(LPCSTR name){
+   typedef WINGDIAPI PROC WINAPI (*ftype)(LPCSTR name);
+   static ftype                    function=NULL;
+
+   if(function==NULL){
+    HANDLE library=LoadLibrary("OPENGL32");
+    
+    function=(ftype)GetProcAddress(library,"wglGetProcAddress");
+   }
+   if(function==NULL)
+    return NULL;
+
+   return function(name);
+}
+
+void  opengl_glReadBuffer(GLenum mode) {
+   typedef WINGDIAPI PROC WINAPI (*ftype)(GLenum mode);
+   static ftype                    function=NULL;
+
+   if(function==NULL){
+    HANDLE library=LoadLibrary("OPENGL32");
+    
+    function=(ftype)GetProcAddress(library,"glReadBuffer");
+   }
+   if(function==NULL)
+    return;
+
+   function(mode);
+}
+
+void opengl_glGetIntegerv(GLenum pname,GLint *params) {
+   typedef WINGDIAPI PROC WINAPI (*ftype)(GLenum pname,GLint *params);
+   static ftype                    function=NULL;
+
+   if(function==NULL){
+    HANDLE library=LoadLibrary("OPENGL32");
+    
+    function=(ftype)GetProcAddress(library,"glGetIntegerv");
+   }
+   if(function==NULL)
+    return;
+
+   function(pname,params);
+}
+
+
+void  opengl_glDrawBuffer(GLenum mode) {
+   typedef WINGDIAPI PROC WINAPI (*ftype)(GLenum mode);
+   static ftype                    function=NULL;
+
+   if(function==NULL){
+    HANDLE library=LoadLibrary("OPENGL32");
+    
+    function=(ftype)GetProcAddress(library,"glDrawBuffer");
+   }
+   if(function==NULL)
+    return;
+
+   function(mode);
+}
+
+void opengl_glReadPixels(GLint x,GLint y,GLsizei width,GLsizei height,GLenum format,GLenum type,GLvoid *pixels) {
+   static typeof(glReadPixels) *function=NULL;
+
+   if(function==NULL){
+    HANDLE library=LoadLibrary("OPENGL32");
+    
+    function=(typeof(function))GetProcAddress(library,"glReadPixels");
+   }
+   if(function==NULL)
+    return;
+
+   function(x,y,width,height,format,type,pixels);
+}
+
+
+
---
+++ CoreFoundation/CFData.m
@@ -0,0 +1,79 @@
+#import <CoreFoundation/CFData.h>
+#import <Foundation/NSRaise.h>
+#import <Foundation/NSData.h>
+#import <Foundation/NSCFTypeID.h>
+
+@interface __CFMutableData : NSMutableData
+@end
+
+static inline NSRange NSRangeFromCFRange(CFRange range){
+   NSRange result={range.location,range.length};
+   return result;
+}
+
+CFTypeID CFDataGetTypeID(void){
+   return kNSCFTypeData;
+}
+
+CFDataRef CFDataCreate(CFAllocatorRef allocator,const uint8_t *bytes,CFIndex length){
+   return [[NSData allocWithZone:NULL] initWithBytes:bytes length:length];
+}
+
+CFDataRef CFDataCreateWithBytesNoCopy(CFAllocatorRef allocator,const uint8_t *bytes,CFIndex length,CFAllocatorRef bytesAllocator){
+   NSUnimplementedFunction();
+   return 0;
+}
+
+CFDataRef CFDataCreateCopy(CFAllocatorRef allocator,CFDataRef self){
+   return [self copy];
+}
+
+CFIndex CFDataGetLength(CFDataRef self){
+   return [self length];
+}
+
+const uint8_t *CFDataGetBytePtr(CFDataRef self){
+   return [self bytes];
+}
+
+void CFDataGetBytes(CFDataRef self,CFRange range,uint8_t *bytes){
+   [self getBytes:bytes range:NSRangeFromCFRange(range)];
+}
+
+// mutable
+
+CFMutableDataRef CFDataCreateMutable(CFAllocatorRef allocator,CFIndex capacity){
+   return [[NSMutableData allocWithZone:NULL] initWithCapacity:capacity];
+}
+
+CFMutableDataRef CFDataCreateMutableCopy(CFAllocatorRef allocator,CFIndex capacity,CFDataRef other){
+   CFMutableDataRef self=CFDataCreateMutable(allocator,capacity);
+   
+   [self setData:other];
+
+   return self;
+}
+
+uint8_t *CFDataGetMutableBytePtr(CFMutableDataRef self){
+   return [self mutableBytes];
+}
+
+void CFDataSetLength(CFMutableDataRef self,CFIndex length){
+   [self setLength:length];
+}
+
+void CFDataAppendBytes(CFMutableDataRef self,const uint8_t *bytes,CFIndex length){
+   [self appendBytes:bytes length:length];
+}
+
+void CFDataDeleteBytes(CFMutableDataRef self,CFRange range){
+   [self replaceBytesInRange:NSRangeFromCFRange(range) withBytes:NULL length:0];
+}
+
+void CFDataIncreaseLength(CFMutableDataRef self,CFIndex delta){
+   [self increaseLengthBy:delta];
+}
+
+void CFDataReplaceBytes(CFMutableDataRef self,CFRange range,const uint8_t *bytes,CFIndex length){
+   [self replaceBytesInRange:NSRangeFromCFRange(range) withBytes:bytes length:length];
+}
