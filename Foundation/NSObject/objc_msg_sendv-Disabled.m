5aef8969ddfeac75328bb0df62f7de2aac84d089
Christopher Lloyd <cjwl@objc.net>
2011-08-16 22:21:34 -0400

    Fix replaying the same sound

---
+++ Foundation/NSObject/objc_msg_sendv-Disabled.m
@@ -0,0 +1,11 @@
+
+#import <Foundation/NSException.h>
+#import <Foundation/NSString.h>
+#import <objc/message.h>
+
+id objc_msgSendv(id self, SEL selector, unsigned arg_size, void *arg_frame)
+{
+	[NSException raise:@"OBJCForwardingUnavailableException" format:@"Sorry, but objc_msgSendv and forwarding including NSInvocation are unavailable on this platform."];
+	return nil;
+}
+
