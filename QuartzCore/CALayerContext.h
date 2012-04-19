7c03e3439528b31e7e55468ad30848652f88e91d
Christopher Lloyd <cjwl@objc.net>
2012-04-19 09:53:44 -0400

    dont save too many events when running a modal

---
+++ QuartzCore/CALayerContext.h
@@ -0,0 +1,33 @@
+#import <Foundation/NSObject.h>
+#import <OpenGL/OpenGL.h>
+#import <CoreGraphics/CGGeometry.h>
+
+@class CARenderer,CALayer,CGLPixelSurface,NSTimer,NSMutableArray,NSNumber;
+
+@interface CALayerContext : NSObject {
+    CGRect _frame;
+   CGLPixelFormatObj _pixelFormat;
+   CGLContextObj     _glContext;
+   CALayer          *_layer;
+   CARenderer       *_renderer;
+   
+   NSMutableArray   *_deleteTextureIds;
+   
+   NSTimer          *_timer;
+}
+
+-initWithFrame:(CGRect)rect;
+
+-(CGLPixelSurface *)pixelSurface;
+-(void)setFrame:(CGRect)value;
+-(void)setLayer:(CALayer *)layer;
+
+-(void)invalidate;
+
+-(void)render;
+
+-(void)startTimerIfNeeded;
+
+-(void)deleteTextureId:(NSNumber *)textureId;
+
+@end
